7d9a0fcc9f256f9e6ef3c4cb7c6988f9cd65a69a
s41ntm4rt1n <bryannmorphy796@gmail.com>
2024-04-09 12:28:36 +0300

    modified password reset email template

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.urls import path
 from .views import *
 from django.contrib.auth import views as auth_views
@@ -10,7 +11,7 @@ urlpatterns=[
     path('accounts/login/', Login, name='login'),
     path('accounts/logout/', Logout, name='logout'),
     
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html', from_email='MS_AIFmlp@robotsaint.com', html_email_template_name='registration/password_reset_email_form.html'), name='reset_password'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html', from_email=settings.EMAIL_HOST_USER, html_email_template_name='registration/password_reset_email_form.html'), name='reset_password'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_form.html'), name='password_reset_confirm'),
     path('accounts/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_complete'),
